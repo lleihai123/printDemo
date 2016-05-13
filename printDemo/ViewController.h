729d71e7ae5dae80da570adea5acaa2efba3b84d
leihai <876732861@qq.com>
2016-05-13 18:04:57 +0800

    test
    test

--- printDemo/ViewController.h
+++ printDemo/ViewController.h
@@ -7,6 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
+typedef  enum {
+    Align_Left = 0X00,
+    Align_Center,
+    Align_Right,
+}Align_Type_e;
+
+typedef  enum {
+    Char_Normal = 0X00,
+    Char_Zoom_2,
+    Char_Zoom_3,
+    Char_Zoom_4
+}Char_Zoom_Num_e;
+
+
+typedef  enum {
+    TICKET_SALE = 1,
+    TICKET_CARD,
+}TYPE_TICKET;
+#define MAX_CHARACTERISTIC_VALUE_SIZE 20
 
 @interface ViewController : UIViewController
 
